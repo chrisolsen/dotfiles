6b8f5c1afd9374514ddb3934b3eb4e786921e75a
Chris Olsen <olsen.chris@gmail.com>
2024-04-22 14:14:57 -0600

    add nix configuration

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,164 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-7c94c1c5-586b-4880-9c40-bf369fac7c81".device = "/dev/disk/by-uuid/7c94c1c5-586b-4880-9c40-bf369fac7c81";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Edmonton";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Hack" "FiraCode" "DroidSansMono" ]; })
+  ];
+
+  # ZSA keyboard
+  hardware.keyboard.zsa.enable = true;
+  users.extraGroups.plugdev = { name = "plugdev"; };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.defaultUserShell = pkgs.fish;
+  users.users.chris = {
+    isNormalUser = true;
+    description = "Chris";
+    extraGroups = [ "networkmanager" "wheel" "plugdev" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      aseprite
+      btop
+      dbeaver
+      dropbox
+      drawio
+      ephemeral
+      firefox
+      inkscape
+      kate
+      krita
+      lazygit
+      lf
+      nerdfonts
+      obsidian
+      onlyoffice-bin
+      peek
+      plex
+      slack
+      thunderbird
+      tmux
+      transmission_4
+      transmission_4-qt
+      vivaldi
+      vlc
+      xsel
+    ];
+  };
+
+  nix.settings.trusted-users = [ "root" "chris" ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # obsidian is dependent on this 2024/2/27
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    helix
+    git
+
+    # https://wiki.nixos.org/wiki/ZSA_Keyboards#Flashing
+    wally-cli  # ZSA keyboard    
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  programs.fish.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
