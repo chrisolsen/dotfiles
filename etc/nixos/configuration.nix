f073c20f7461bf1a14c88ae4087ffd8f78867f28
Chris Olsen <olsen.chris@gmail.com>
2024-06-24 09:18:40 -0600

    24.05 required fixes

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -35,13 +35,14 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
   };
 
   # Enable CUPS to print documents.
