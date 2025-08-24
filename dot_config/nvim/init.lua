598d5377db09c23b543cb58e371a410e7673859f
Chris Olsen <olsen.chris@gmail.com>
2025-08-24 14:26:43 -0600

    add lsps to neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -682,7 +682,6 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -690,8 +689,16 @@ require("lazy").setup({
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
+				gopls = {},
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
+				ts_ls = {},
+				svelte = {},
+				cssls = {},
+				html = {},
+				htmx = {},
+				prettier = {},
+				eslint = {},
+				emmet_ls = {},
 				--
 
 				lua_ls = {
