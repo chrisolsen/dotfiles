01b71768b6ba0770f4a448fdbbbd4147bc403eeb
Chris Olsen <olsen.chris@gmail.com>
2025-09-10 09:16:09 -0600

    add default vim tab settings

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.colorcolumn = "80"
+vim.cmd("syntax enable")
+vim.api.nvim_command("filetype plugin indent on")
+
 return {}
