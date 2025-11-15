3bf34b75c3817a93dc35d1a7bc5be4eb8808f26f
chrisolsen <olsen.chris@gmail.com>
2025-11-14 23:28:16 -0700

    ensure custom scripts are executable

--- dot_local/scripts/executable_fzl_common.sh
+++ dot_local/scripts/executable_fzl_common.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-echo -e 'Network config' | fuzzel --dmenu -l 1 --width 11 | {
+echo -e 'Network config\nSystem monitor' | fuzzel --dmenu -l 2 --width 11 | {
 	read option;
 	case $option in
 		'Network config')
 			ghostty -e bash -c nmtui
+		;;
+
+		'System monitor')
+			ghostty -e bash -c btop
 		;;
 	esac
 }
